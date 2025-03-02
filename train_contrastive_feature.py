c8bc38cadd2bbb3eee8fdb728ece8d4426c48aa8
Arnav Varshney <varshney.arnav@gmail.com>
2025-03-02 20:03:45 +0800

    bug fix

--- train_contrastive_feature.py
+++ train_contrastive_feature.py
@@ -69,6 +69,8 @@ def training(dataset, opt, pipe, iteration, saving_iterations, checkpoint_iterat
 
     dataset.need_features = False
     dataset.need_masks = True
+    dataset.feature_dim = 32
+    dataset.allow_principle_point_shift = False
 
     gaussians = GaussianModel(dataset.sh_degree)
 
