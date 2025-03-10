39e708c0342520389622735c70589f50c5904c94
Arnav Varshney <varshney.arnav@gmail.com>
2025-03-11 03:29:53 +0800

    Refactor render function to simplify rendering logic and improve code clarity

--- render.py
+++ render.py
@@ -30,73 +30,29 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
     makedirs(gts_path, exist_ok=True)
     makedirs(mask_path, exist_ok=True)
 
-    if target == 'feature':
-        render_func = render_contrastive_feature
-    elif target == 'contrastive_feature':
-        render_func = render_contrastive_feature
-    elif target == 'xyz':
-        render_func = render
-    else:
-        render_func = render
-
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
-
-        res = render_func(view, gaussians, pipeline, background)
-
-        if target == 'seg':
-            assert precomputed_mask is not None and 'Rendering 2D segmentation mask requires a precomputed mask.'
-            mask_res = render_mask(view, gaussians, pipeline, background, precomputed_mask=precomputed_mask)
-
+        res = render(view, gaussians, pipeline, background)
         rendering = res["render"]
-        gt = view.original_image[0:3, :, :]
-        torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
-        if target == 'seg':
-            mask = mask_res["mask"]
-            mask[mask < 0.5] = 0
-            mask[mask != 0] = 1
-            mask = mask[0, :, :]
-            torchvision.utils.save_image(mask, os.path.join(mask_path, '{0:05d}'.format(idx) + ".png"))
-        if target == 'seg' or target == 'scene':
+        if target == 'scene':
             torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
-        elif 'feature' in target:
-            torch.save(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".pt"))
-        elif target == 'xyz':
-            torch.save(rendering, os.path.join(render_path, 'xyz_{0:05d}'.format(idx) + ".pt"))
-        
         
         
 
 def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, skip_train : bool, skip_test : bool, segment : bool = False, target = 'scene', idx = 0, precomputed_mask = None):
     dataset.need_features = dataset.need_masks = False
-    if segment:
-        assert target == 'seg' or target == 'coarse_seg_everything' or precomputed_mask is not None and "Segmentation only works with target seg!"
     gaussians, feature_gaussians = None, None
     with torch.no_grad():
         if precomputed_mask is not None:
             if '.pt' in precomputed_mask:
                 precomputed_mask = torch.load(precomputed_mask)
-            elif '.npy' in precomputed_mask:
-                import numpy as np
-                precomputed_mask = torch.from_numpy(np.load(precomputed_mask)).cuda()
-                precomputed_mask[precomputed_mask > 0] = 1
-                precomputed_mask[precomputed_mask != 1] = 0
-                precomputed_mask = precomputed_mask.bool()
 
-        if target == 'scene' or target == 'seg' or target == 'coarse_seg_everything' or target == 'xyz':
-            gaussians = GaussianModel(dataset.sh_degree)
-        if target == 'feature' or target == 'coarse_seg_everything' or target == 'contrastive_feature':
-            feature_gaussians = FeatureGaussianModel(dataset.feature_dim)
+        gaussians = GaussianModel(dataset.sh_degree)
 
         scene = Scene(dataset, gaussians, feature_gaussians, load_iteration=iteration, shuffle=False, mode='eval', target=target if target != 'xyz' and precomputed_mask is None else 'scene')
 
-        if segment:
-            gaussians.segment(precomputed_mask)
+        gaussians.segment(precomputed_mask)
 
         bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
-        if 'feature' in target:
-            gaussians = feature_gaussians
-            bg_color = [1 for i in range(dataset.feature_dim)] if dataset.white_background else [0 for i in range(dataset.feature_dim)]
-
         background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
         if not skip_train:
