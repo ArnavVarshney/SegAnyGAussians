e410d9ce1833a9430a78cb54d9a5f718474f2820
Arnav Varshney <varshney.arnav@gmail.com>
2025-03-11 00:16:20 +0800

    Add env setup script

---
+++ setup_environment.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+. $HOME/miniconda3/etc/profile.d/conda.sh
+
+echo "Setting up environment"
+conda env create -f environment.yml
+conda activate saga
+
+echo "Setting up environment variables"
+echo "CONDA_PREFIX: ${CONDA_PREFIX}"
+conda env config vars set CUDA_HOME=${CONDA_PREFIX}
+conda env config vars set LD_LIBRARY_PATH=${CONDA_PREFIX}/targets/x86_64-linux/include/lib
+conda env config vars set CPATH=${CONDA_PREFIX}/targets/x86_64-linux/include
+conda deactivate
+
+echo "Installing pip dependencies"
+conda activate saga
+pip install -e submodules/diff-gaussian-rasterization
+pip install -e submodules/diff-gaussian-rasterization_contrastive_f
+pip install -e submodules/diff-gaussian-rasterization-depth
+pip install -e third_party/segment-anything
+pip install git+https://github.com/facebookresearch/pytorch3d.git@stable
+
+echo "Installing versioned pip dependencies"
+pip install joblib==1.1.0 numpy==2.0
