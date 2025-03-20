e7b1de159ec092120ae87f913bee7b44fd3cac45
Arnav Varshney <varshney.arnav@gmail.com>
2025-03-20 23:32:09 +0800

    Add sbatch script for training models and update environment setup

--- setup_environment.sh
+++ setup_environment.sh
@@ -32,3 +32,7 @@ pip install git+https://github.com/facebookresearch/pytorch3d.git@stable
 
 echo "Installing versioned pip dependencies"
 pip install joblib==1.1.0
+
+echo "Downloading pre-trained models"
+mkdir ./submodules/segment-anything/sam_ckpt
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth -O ./submodules/segment-anything/sam_ckpt/sam_vit_h_4b8939.pth
