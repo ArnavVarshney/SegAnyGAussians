2e157167580939409f6de26c7e4a5ec6485866a9
Arnav Varshney <varshney.arnav@gmail.com>
2025-03-30 00:54:22 +0800

    Refactor rendering process to support cluster-based rendering and improve mask handling

--- render.py
+++ render.py
@@ -10,11 +10,11 @@
 #
 
 import torch
-from scene import Scene, GaussianModel, FeatureGaussianModel
+from scene import Scene, GaussianModel
 import os
 from tqdm import tqdm
 from os import makedirs
-from gaussian_renderer import render, render_contrastive_feature, render_mask
+from gaussian_renderer import old_render, render
 import torchvision
 
 from utils.general_utils import safe_state
@@ -22,76 +22,34 @@ from argparse import ArgumentParser
 from arguments import ModelParams, PipelineParams, get_combined_args
 from gaussian_renderer import GaussianModel
 
-try:
-    from diff_gaussian_rasterization import SparseGaussianAdam
 
-    SPARSE_ADAM_AVAILABLE = True
-except:
-    SPARSE_ADAM_AVAILABLE = False
-
-
-def render_set(
-    model_path,
-    name,
-    iteration,
-    views,
-    gaussians,
-    pipeline,
-    background,
-    train_test_exp,
-    separate_sh,
-    target,
-    precomputed_mask=None,
-):
-    render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
-    gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
-    mask_path = os.path.join(model_path, name, "ours_{}".format(iteration), "mask")
-
-    makedirs(render_path, exist_ok=True)
-    makedirs(gts_path, exist_ok=True)
-    makedirs(mask_path, exist_ok=True)
-
-    for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
-        rendering = render(
-            view,
-            gaussians,
-            pipeline,
-            background,
-            use_trained_exp=train_test_exp,
-            separate_sh=separate_sh,
-        )["render"]
-        gt = view.original_image[0:3, :, :]
-
-        if args.train_test_exp:
-            rendering = rendering[..., rendering.shape[-1] // 2 :]
-            gt = gt[..., gt.shape[-1] // 2 :]
-
-        torchvision.utils.save_image(
-            rendering, os.path.join(render_path, "{0:05d}".format(idx) + ".png")
-        )
-        torchvision.utils.save_image(
-            gt, os.path.join(gts_path, "{0:05d}".format(idx) + ".png")
-        )
-
-
-def render_sets(
+def render_clusters(
     dataset: ModelParams,
     iteration: int,
     pipeline: PipelineParams,
-    skip_train: bool,
-    skip_test: bool,
-    separate_sh: bool,
-    segment: bool = False,
-    target="scene",
-    idx=0,
-    precomputed_mask=None,
+    clusters_root=None,
 ):
     dataset.need_features = dataset.need_masks = False
     gaussians, feature_gaussians = None, None
+
     with torch.no_grad():
-        if precomputed_mask is not None:
-            if ".pt" in precomputed_mask:
-                precomputed_mask = torch.load(precomputed_mask)
+        if not os.path.exists(args.clusters_root):
+            print(f"No cluster masks found at {args.clusters_root}.")
+            return
+        else:
+            cluster_dirs = sorted(
+                [
+                    int(d)
+                    for d in os.listdir(args.clusters_root)
+                    if os.path.isdir(os.path.join(args.clusters_root, d))
+                ]
+            )
+            if not cluster_dirs:
+                print("No cluster directories found.")
+                return
+            print(
+                f"Found {len(cluster_dirs)} cluster masks. Rendering images for each..."
+            )
 
         gaussians = GaussianModel(dataset.sh_degree)
 
@@ -102,43 +60,37 @@ def render_sets(
             load_iteration=iteration,
             shuffle=False,
             mode="eval",
-            target=target if target != "xyz" and precomputed_mask is None else "scene",
+            target="scene",
         )
 
-        gaussians.segment(precomputed_mask)
+        for cluster_id in tqdm(cluster_dirs, desc="Rendering clusters"):
+            mask_path = os.path.join(clusters_root, str(cluster_id), "mask.pt")
+            if not os.path.exists(mask_path):
+                print(f"No mask found for cluster {cluster_id}, skipping...")
+                continue
 
-        bg_color = [1, 1, 1] if dataset.white_background else [0, 0, 0]
-        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+            mask = torch.load(mask_path)
+            gaussians.segment(mask)
 
-        if not skip_train:
-            render_set(
-                dataset.model_path,
-                "train",
-                scene.loaded_iter,
-                scene.getTrainCameras(),
-                gaussians,
-                pipeline,
-                background,
-                dataset.train_test_exp,
-                separate_sh,
-                target,
-                precomputed_mask=precomputed_mask,
-            )
+            bg_color = [1, 1, 1] if dataset.white_background else [0, 0, 0]
+            background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
-        if not skip_test:
-            render_set(
-                dataset.model_path,
-                "test",
-                scene.loaded_iter,
-                scene.getTestCameras(),
-                gaussians,
-                pipeline,
-                background,
-                dataset.train_test_exp,
-                separate_sh,
-                target,
-                precomputed_mask=precomputed_mask,
-            )
+            for view in scene.getTrainCameras():
+                rendering = old_render(
+                    view,
+                    gaussians,
+                    pipeline,
+                    background,
+                )["render"]
+
+                torchvision.utils.save_image(
+                    rendering,
+                    os.path.join(
+                        clusters_root, str(cluster_id), f"{view.image_name}.png"
+                    ),
+                )
+
+            gaussians.clear_segment()
 
 
 if __name__ == "__main__":
@@ -146,50 +98,24 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Testing script parameters")
     model = ModelParams(parser, sentinel=True)
     pipeline = PipelineParams(parser)
-    parser.add_argument("--iteration", default=-1, type=int)
-    parser.add_argument("--skip_train", action="store_true")
-    parser.add_argument("--skip_test", action="store_true")
-    parser.add_argument("--quiet", action="store_true")
-    parser.add_argument("--segment", action="store_true")
     parser.add_argument(
         "--target",
         default="scene",
-        const="scene",
-        nargs="?",
-        choices=[
-            "scene",
-            "seg",
-            "feature",
-            "coarse_seg_everything",
-            "contrastive_feature",
-            "xyz",
-        ],
     )
-    parser.add_argument("--idx", default=0, type=int)
-    parser.add_argument("--precomputed_mask", default=None, type=str)
+    parser.add_argument(
+        "--clusters_root", default="./segmentation_res/clusters/", type=str
+    )
 
     args = get_combined_args(parser)
     print("Rendering " + args.model_path)
 
-    if not hasattr(args, "precomputed_mask"):
-        args.precomputed_mask = None
-    if args.precomputed_mask is not None:
-        print("Using precomputed mask " + args.precomputed_mask)
-
     args.depths = ""
+    args.iteration = -1
     args.train_test_exp = False
-    # Initialize system state (RNG)
-    safe_state(args.quiet)
 
-    render_sets(
+    render_clusters(
         model.extract(args),
         args.iteration,
         pipeline.extract(args),
-        args.skip_train,
-        args.skip_test,
-        SPARSE_ADAM_AVAILABLE,
-        args.segment,
-        args.target,
-        args.idx,
-        args.precomputed_mask,
+        args.clusters_root,
     )
